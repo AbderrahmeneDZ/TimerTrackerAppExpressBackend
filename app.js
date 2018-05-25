daec4a05607aa52365dca8373e2e997bbefe3102
Bouidia Abderrahmene <AbderrahmeneDZ@users.noreply.github.com>
2018-05-25 23:49:38 +0200

    second commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('It works')
+    res.send('It works, yeah sure')
 })
 
 const port = process.env.PORT || 5000;
